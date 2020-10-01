b06ff4dfb482fa43390f2a72a4ffb2235bf44b22
arminv1001 <69273468+arminv1001@users.noreply.github.com>
2020-10-01 21:40:34 +0200

    Hausaufgabe 1

---
+++ Hausaufgabe1.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Persons
+(
+    PersonID  int          NOT NULL,
+    Firstname varchar(255) NOT NULL,
+    Lastname  varchar(255) NOT NULL,
+    Age       int,
+    PRIMARY KEY (PersonID)
+)
