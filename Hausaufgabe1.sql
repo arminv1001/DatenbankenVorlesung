4cde24883358989b27cb4b936ccc8bb2f151c1c5
Armin <69273468+arminv1001@users.noreply.github.com>
2020-10-06 09:47:21 +0200

    Add files via upload

--- Hausaufgabe1.sql
+++ Hausaufgabe1.sql
@@ -5,4 +5,10 @@ CREATE TABLE Persons
     Lastname  varchar(255) NOT NULL,
     Age       int,
     PRIMARY KEY (PersonID)
-)
+);
+Insert into Persons Values (1,"Armin","V", 19);
+Insert into Persons Values (2,"Arian","V", 17);
+Insert into Persons Values (3,"Finn","F", 23);
+Insert into Persons Values (4,"Khiemi","N", 19);
+Insert into Persons Values (5,"Yusuf","S", 20);
+
