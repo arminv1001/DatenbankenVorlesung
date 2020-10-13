78fc48c2b274f5c937d2f23ebed27b263016b5d5
Eikeike <eike@wobken.de>
2020-10-13 16:29:55 +0200

    Ordner hinzugefügt

---
+++ Hausaufgabe_1/Hausaufgabe1.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Persons
+(
+    PersonID  int          NOT NULL,
+    Firstname varchar(255) NOT NULL,
+    Lastname  varchar(255) NOT NULL,
+    Age       int,
+    PRIMARY KEY (PersonID)
+);
+Insert into Persons Values (1,"Armin","V", 19);
+Insert into Persons Values (2,"Arian","V", 17);
+Insert into Persons Values (3,"Finn","F", 23);
+Insert into Persons Values (4,"Khiemi","N", 20);
+Insert into Persons Values (5,"Emmelie","B", 19);
+
